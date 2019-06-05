d43dd7de79f320f1652fedaf3546ad1f6d51b880
ESX-TRADUCIDOS <51387481+Esxspain@users.noreply.github.com>
2019-06-05 15:33:59 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,91 @@
+var tableauQuestion = [
+	{
+		question: "Si vas a 80 KM/H Y te aproximas a una zona residencial debes:",
+		propositionA: "Aceleras",
+		propositionB: "Mantener su velocidad",
+		propositionC: "usted disminuye la velocidad",
+		propositionD: "Doblar y salir de la zona residencial",
+		reponse: "C"
+	},
+
+	{
+		question: "Si está girando a la derecha en un semáforo, pero vea un cruce de peatones, ¿qué hace?:",
+		propositionA: "Pasas al peaton",
+		propositionB: "Comprueba que no hay otros vehículos alrededor.",
+		propositionC: "Esperas hasta que el peatón haya cruzado",
+		propositionD: "Disparas al peatón y sigues conduciendo.",
+		reponse: "C"
+	},
+
+	{
+		question: "Sin ninguna indicación previa, la velocidad en una zona residencial es de: __ km / H",
+		propositionA: "30 km/h",
+		propositionB: "50 km/h",
+		propositionC: "40 km/h",
+		propositionD: "60 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Antes de cada cambio de carril debes:",
+		propositionA: "Chocas al de alado",
+		propositionB: "Revisa tus puntos ciegos",
+		propositionC: "Te paras",
+		propositionD: "Revisas los espejos y los puntos ciegos",
+		reponse: "D"
+	},
+
+	{
+		question: "¿Qué nivel de alcohol en sangre se clasifica como conducir mientras está intoxicado??",
+		propositionA: "0.05%",
+		propositionB: "0.18%",
+		propositionC: "0.08%",
+		propositionD: "0.06%",
+		reponse: "C"
+	},
+
+	{
+		question: "¿Cuándo puedes continuar conduciendo en un semáforo??",
+		propositionA: "Cuando se te de la puta gana",
+		propositionB: "Cuando no hay nadie en la intersección.",
+		propositionC: "Estas en una zona escolar",
+		propositionD: "Cuando está verde y / o rojo y estás girando a la derecha",
+		reponse: "D"
+	},
+
+	{
+		question: "Un peatón tiene una señal de no cruzar, ¿qué haces??",
+		propositionA: "Le dejas pasar",
+		propositionB: "Observas antes de continuar",
+		propositionC: "Les saludas para decirles que crucen",
+		propositionD: "Continúas porque tu semáforo está en verde.",
+		reponse: "D"
+	},
+
+	{
+		question: "Lo que está permitido al pasar a un vehiculo.",
+		propositionA: "Lo sigues de cerca para pasarlo más rápido.",
+		propositionB: "Se pasa sin salir de la calzada.",
+		propositionC: "You drive on the opposite side of the road to pass",
+		propositionD: "Se supera el límite de velocidad para pasarlos",
+		reponse: "C"
+	},
+
+	{
+		question: "Usted está conduciendo en una carretera que indica una velocidad máxima de 120 km / h. Pero la mayoría de los traficantes conducen a 125 km / h, por lo que no debe conducir más rápido que:",
+		propositionA: "120 km/h",
+		propositionB: "125 km/h",
+		propositionC: "130 km/h",
+		propositionD: "110 km/h",
+		reponse: "A"
+	},
+
+	{
+		question: "Cuando es superado por otro vehículo, es importante NO:",
+		propositionA: "Ve más despacio",
+		propositionB: "Revisa tus espejos",
+		propositionC: "Mira a otros conductores",
+		propositionD: "Aumenta tu velocidad",
+		reponse: "D"
+	},
+]
